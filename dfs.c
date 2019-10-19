411b49fadc187fbef39b47795ace8c97709b019a
Jonathan <rabadunk@gmail.com>
2019-10-20 06:06:10 +1300

    still using general directions but now have them displaying, still need to convert this to robot friendly commands

--- dfs.c
+++ dfs.c
@@ -18,7 +18,7 @@ int visited[15][19] = {
     {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 
-int *path[200];
+char *path[200];
 
 int rows = 15;
 int cols = 19;
@@ -43,45 +43,6 @@ enum directions
     start,
     reverse
 };
-
-void print_visited()
-{
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            printf("%d", visited[i][j]);
-        }
-
-        printf("\n");
-    }
-
-    printf("\n");
-}
-
-int dfs(int row, int col, int prevRow, int prevCol, int direction, int prev_direction)
-{
-    int *current = &visited[row][col];
-
-    if (*current == empty)
-    {
-        *current = crumb;
-        path[pathIndex] = direction;
-        pathIndex++;
-
-        int sum = 0;
-
-        printf("row: %d, column: %d, direction: %d\n", row, col, direction);
-        dfs(row, col - 1, row, col, left, opp_direction(direction));
-        dfs(row + 1, col, row, col, down, opp_direction(direction));
-        dfs(row, col + 1, row, col, right, opp_direction(direction));
-        dfs(row - 1, col, row, col, up, opp_direction(direction));
-        printf("prev row: %d, prev column: %d, prev direction: %d \n", prevRow, prevCol, prev_direction);
-    }
-
-    return 0;
-}
 
 int opp_direction(int direction)
 {
@@ -108,17 +69,88 @@ int opp_direction(int direction)
     return up;
 }
 
+void print_visited()
+{
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d", visited[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    printf("\n");
+}
+
+void add_to_path(int direction)
+{
+
+    switch (direction)
+    {
+    case up:
+        path[pathIndex] = 'u';
+        pathIndex++;
+        break;
+    case down:
+        path[pathIndex] = 'd';
+        pathIndex++;
+        break;
+    case left:
+        path[pathIndex] = 'l';
+        pathIndex++;
+        break;
+
+    case right:
+        path[pathIndex] = 'r';
+        pathIndex++;
+        break;
+    case reverse:
+        path[pathIndex] = 'u';
+        pathIndex++;
+        break;
+    };
+}
+
+int dfs(int row, int col, int prevRow, int prevCol, int direction, int prev_direction)
+{
+    int *current = &visited[row][col];
+
+    if (*current == empty)
+    {
+        *current = crumb;
+
+        add_to_path(direction);
+
+        int sum = 0;
+
+        //printf("row: %d, column: %d, direction: %d\n", row, col, direction);
+        dfs(row, col - 1, row, col, left, opp_direction(direction));
+        dfs(row + 1, col, row, col, down, opp_direction(direction));
+        dfs(row, col + 1, row, col, right, opp_direction(direction));
+        dfs(row - 1, col, row, col, up, opp_direction(direction));
+
+        add_to_path(prev_direction);
+
+        //printf("prev row: %d, prev column: %d, prev direction: %d \n", prevRow, prevCol, prev_direction);
+    }
+
+    return 0;
+}
+
 void print_path()
 {
     for (int i = 0; i < 200; i++)
     {
-        printf("Direction: %d \n", path[i]);
+        printf("Direction: %c \n", path[i]);
     }
 }
 
 void alloc_path()
 {
-    *path = malloc(200 * sizeof(int *));
+    *path = malloc(200 * sizeof(char *));
 }
 
 int main()
@@ -127,7 +159,7 @@ int main()
     // print_visited();
     dfs(start_row, start_col, 0, 0, start, start);
     // print_visited();
-    // print_path();
+    print_path();
 
     return 0;
 }
