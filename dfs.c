d8685d8b13569076e2f38e98f2598966b3c1d057
Joey Miller <9375730+itsmejoeeey@users.noreply.github.com>
2019-10-22 19:29:40 +1300

    Final depth search code

--- dfs.c
+++ dfs.c
@@ -89,13 +89,13 @@ int dfs(int row, int col, int prevRow, int prevCol, int direction, int prev_dire
             *current = crumbed_intersection;
         }
 
-        printf("%d %d,", row, col);
+        printf("%d %d,\n", row, col);
         add_coords(row, col);
 
         if (dfs(row, col - 1, row, col, left, opp_direction(direction)))
         {
             *current = me;
-            printf("%d %d,", row, col - 1);
+            printf("%d %d,\n", row, col - 1);
             add_coords(row, col - 1);
             return 1;
         }
@@ -103,14 +103,14 @@ int dfs(int row, int col, int prevRow, int prevCol, int direction, int prev_dire
         if (dfs(row + 1, col, row, col, down, opp_direction(direction)))
         {
             *current = me;
-            printf("%d %d,", row + 1, col);
+            printf("%d %d,\n", row + 1, col);
             add_coords(row + 1, col);
             return 1;
         }
 
         if (dfs(row, col + 1, row, col, right, opp_direction(direction)))
         {
-            printf("%d %d,", row, col + 1);
+            printf("%d %d,\n", row, col + 1);
             add_coords(row, col + 1);
             *current = me;
             return 1;
@@ -118,7 +118,7 @@ int dfs(int row, int col, int prevRow, int prevCol, int direction, int prev_dire
 
         if (dfs(row - 1, col, row, col, up, opp_direction(direction)))
         {
-            printf("%d %d,", row - 1, col);
+            printf("%d %d, \n", row - 1, col);
             add_coords(row - 1, col);
             *current = me;
             return 1;
@@ -128,18 +128,170 @@ int dfs(int row, int col, int prevRow, int prevCol, int direction, int prev_dire
 
         if (*current == crumb || *current == crumbed_intersection)
         {
-            printf("%d %d,", prevRow, prevCol);
+            printf("%d %d \n,", prevRow, prevCol);
             add_coords(prevRow, prevCol);
         }
     }
 
     return 0;
 }
+#define REMOVED_VALUE 256
+
+int CheckIntersection(int gridY, int gridX) {
+	if (map[gridY][gridX] == 4) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+	/*
+	GetOpenNeighbours(gridY, gridX);
+	int count = 0;
+	for (int i = 0; i < 9; i++) {
+		if (neighbours[i][0] != REMOVED_VALUE && neighbours[i][1] != REMOVED_VALUE) {
+			count++;
+		}
+	}
+	if (count > 2) {
+		return 1;
+	}
+	if (count == 2) {
+		int nodeX = -1;
+		int nodeY = -1;
+		for (int i = 0; i < 9; i++) {
+			if (neighbours[i][0] != REMOVED_VALUE && neighbours[i][1] != REMOVED_VALUE) {
+				if (nodeX == -1 && nodeY == -1) {
+					nodeY = neighbours[i][0];
+					nodeX = neighbours[i][1];
+				}
+				else {
+					if (nodeY != neighbours[i][0] && nodeX != neighbours[i][1]) {
+						return 1;
+					}
+				}
+			}
+		}
+	}
+	return 0;
+	*/
+}
+
+
+//LOOK HERE BOIS --!!!!!!!!!!!!!!!!!!!!
+enum instructions directions[64];
+
+int finalDirectionY = 1;
+int finalDirectionX = 0;
+
+const char* getDirectionName(enum instruction direction)
+{
+	switch (direction)
+	{
+	case Straight: return "Straight";
+	case Left: return "Left";
+	case Right: return "Right";
+	case Stop: return "Stop";
+	case UTurn: return "UTurn";
+	}
+}
+//Pass in an array of node and it will convert to directions
+void ConvertPathToDirection() {
+	int i = 0;
+	int j = 0;
+	int prevDeltaX = finalDirectionX;
+	int prevDeltaY = finalDirectionY;
+	int deltaY;
+	int deltaX;
+	int prevIntersectY = 0;
+	int prevIntersectX = 0;
+	while (i != coordsIndex) {
+		deltaY = rowcoords[i + 1] - rowcoords[i];
+		deltaX = colcoords[i + 1] - colcoords[i];
+
+		if (CheckIntersection(rowcoords[i], colcoords[i]) == 0) {
+			i++;
+			continue;
+		}
+
+		if (prevIntersectY == rowcoords[i] && prevIntersectX == colcoords[i]) {
+			int prevCordX = colcoords[i - 1];
+			int prevCordY = rowcoords[i - 1];
+
+			directions[j] = UTurn;
+			printf("%s \n", getDirectionName(directions[j]));
+			j++;
+			prevDeltaY = rowcoords[i] - prevCordY;
+			prevDeltaX = colcoords[i] - prevCordX;
+		}
+
+		prevIntersectY = rowcoords[i];
+		prevIntersectX = colcoords[i];
+
+		if (i + 1 == coordsIndex) {
+			i++;
+			continue;
+		}
+		if (deltaY == -prevDeltaY && deltaY != 0) {
+			directions[j] = UTurn;
+			prevDeltaX = deltaX;
+			prevDeltaY = deltaY;
+			printf("%s \n", getDirectionName(directions[j]));
+			i++;
+			j++;
+			continue;
+		}
+		else if (deltaX == -prevDeltaX && deltaX != 0) {
+			directions[j] = UTurn;
+			prevDeltaX = deltaX;
+			prevDeltaY = deltaY;
+			printf("%s \n", getDirectionName(directions[j]));
+			i++;
+			j++;
+			continue;
+		}
+		if (deltaY == 1 && prevDeltaX == 1) {
+			directions[j] = Right;
+		}
+		else if (deltaY == 1 && prevDeltaX == -1) {
+			directions[j] = Left;
+		}
+		else if (deltaY == -1 && prevDeltaX == -1) {
+			directions[j] = Right; //CHANGE THIS LOGIC PLS
+		}
+		else if (deltaY == -1 && prevDeltaX == 1) {
+			directions[j] = Left;
+		}
+		else if (deltaX == 1 && prevDeltaY == -1) {
+			directions[j] = Right;
+		}
+		else if (deltaX == -1 && prevDeltaY == 1) {
+			directions[j] = Right;
+		}
+		else if (deltaX == -1 && prevDeltaY == -1) {
+			directions[j] = Left;
+		}
+		else if (deltaX == 1 && prevDeltaY == 1) {
+			directions[j] = Left;
+		}
+		else {
+			directions[j] = Straight;
+		}
+		printf("%s \n", getDirectionName(directions[j]));
+		prevDeltaX = deltaX;
+		prevDeltaY = deltaY;
+		i++;
+		j++;
+	}
+	i -= 2;
+	finalDirectionY = rowcoords[i + 1] - rowcoords[i];
+	finalDirectionX = colcoords[i + 1] - colcoords[i];
+}
 
 int main()
 {
     print_maze();
     dfs(start_row, start_col, 0, 0, down, start);
+	ConvertPathToDirection();
     print_coords();
     print_maze();
 
